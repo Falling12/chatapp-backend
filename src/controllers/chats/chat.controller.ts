fd00d9f1f38c2ff1e9b2fd0d7731948ed15283df
falling12 <senkcsani@gmail.com>
2023-12-30 17:46:21 +0100

    create chat

--- src/controllers/chats/chat.controller.ts
+++ src/controllers/chats/chat.controller.ts
@@ -9,17 +9,54 @@ export const getUserChats = async (req: IRequestWithUser, res: Response) => {
 
     const chats = await prisma.chat.findMany({
         where: {
-            Users: {
+            users: {
                 some: {
                     id: req.user?.id
                 }
             }
         },
         include: {
-            Users: true,
-            Messages: true
+            users: {
+                select: {
+                    id: true,
+                    name: true,
+                    email: true
+                }
+            },
+            messages: true
         }
     })
 
     return res.json({ data: chats, success: true })
+}
+
+export const createNewChat = async (req: IRequestWithUser, res: Response) => {
+    const { name, userIds } = req.body
+
+    try {
+        const chat = await prisma.chat.create({
+            data: {
+                name: name,
+                users: {
+                    connect: userIds.map((id: string) => ({ id }))
+                }
+            },
+            include: {
+                users: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                },
+                messages: true
+            }
+        })
+
+        return res.json({ data: chat, success: true })
+    } catch (error) {
+        console.log(error)
+
+        return res.status(500).json({ message: 'Error creating chat', success: false })
+    }
 }
--- src/routes/chats/chats.ts
+++ src/routes/chats/chats.ts
@@ -1,8 +1,9 @@
 import Router from 'express';
-import { getUserChats } from '../../controllers/chats/chat.controller';
+import { createNewChat, getUserChats } from '../../controllers/chats/chat.controller';
 
 const router = Router();
 
 router.get('/', async (req, res) => getUserChats(req, res))
+router.post('/', async (req, res) => createNewChat(req, res))
 
 export default router;
